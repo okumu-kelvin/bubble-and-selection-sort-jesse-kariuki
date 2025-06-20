65af472698116c6b28ee72ad8e06f1ed92406878
inthezonewithme <158035989+inthezonewithme@users.noreply.github.com>
2025-06-20 10:00:25 +0300

    sorting

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,12 @@
 def bubble_sort(unsorted_list):
     # TODO: Implement bubble sort
-    pass
+    for i in range(len(unsorted_list)):
+        for j in range(len(unsorted_list)-i-1):
+            if unsorted_list[j] > unsorted_list[j + 1]:
+                temp = unsorted_list[j]
+                unsorted_list[j] = unsorted_list[j + 1]
+                unsorted_list[j + 1] = temp
+
+    return unsorted_list
+
+print(bubble_sort([15, 6, 17, 8, 19, 10,1]))
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,16 @@
 def selection_sort(arr):
     # TODO: Implement selection sort
-    pass
+
+    n= len(arr)
+
+    for i in range(n):
+        min = i
+        for j in range(i+1,n):
+            if arr[j] < arr[min]:
+                min = j
+
+            (arr[i],arr[min]) = (arr[min],arr[i])
+
+    return arr
+
+print(selection_sort([3,2,1,5,6,7,8,9,10]))
