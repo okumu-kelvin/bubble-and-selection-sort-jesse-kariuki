6657adfd9b1b1fd1cc6bede96d24f3f4b095c73e
inthezonewithme <158035989+inthezonewithme@users.noreply.github.com>
2025-06-20 10:08:48 +0300

    sorting

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,16 +1,13 @@
 def selection_sort(arr):
     # TODO: Implement selection sort
-
-    n= len(arr)
-
+    n = len(arr)
     for i in range(n):
         min = i
-        for j in range(i+1,n):
+        for j in range(i+1, n):
             if arr[j] < arr[min]:
                 min = j
 
-            (arr[i],arr[min]) = (arr[min],arr[i])
-
+        temp = arr[i]
+        arr[i] = arr[min]
+        arr[min] = temp
     return arr
-
-print(selection_sort([3,2,1,5,6,7,8,9,10]))
